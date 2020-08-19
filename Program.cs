d058fa27b3a75111168aaf9fb08620e421a82e68
katoridanh08 <52204681+katoridanh08@users.noreply.github.com>
2020-08-19 13:45:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.M.A004.Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,y;
+            Console.WriteLine("Nhap x = ");
+            x = int.Parse(Console.ReadLine());
+            y = 2 * x * x * x - 6 * x * x + 2 * x  -1;
+            Console.WriteLine(y);
+        }
+    }
+}
